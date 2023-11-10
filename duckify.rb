6fad4e510447514b62eb98e65b01a0524e403147
Matt Frutchey （マット・フルシェー） <108436172+mjfru@users.noreply.github.com>
2023-11-10 07:52:20 -0500

    Add files via upload
    (Previously uploaded in it's own repo) - A small project working with control flow and string methods in Ruby.

---
+++ duckify.rb
@@ -0,0 +1,12 @@
+print "Thtring, pleathe!: "
+user_input = gets.chomp
+user_input.downcase!
+if user_input.empty?
+    print "Hey, forgetting something? Run this program again and enter a word!"
+elsif user_input.include? "s"
+    puts "S found!"
+    user_input.gsub!(/s/, "th")
+    puts "Your duckified word is: #{user_input}!"
+else
+    print "No 's' found here!"
+end
