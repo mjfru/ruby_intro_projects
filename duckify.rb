3d669f92b56d85f2c6d45ceda0cb594328fdcb3a
Matt Frutchey （マット・フルシェー） <108436172+mjfru@users.noreply.github.com>
2023-12-02 15:21:47 -0500

    fix typo

--- duckify.rb
+++ duckify.rb
@@ -4,7 +4,7 @@ user_input.downcase!
 if user_input.empty?
     print "Hey, forgetting something? Run this program again and enter a word!"
 elsif user_input.include? "s"
-    puts "S found!"
+    puts "An 'S' was found!"
     user_input.gsub!(/s/, "th")
     puts "Your duckified word is: #{user_input}!"
 else
